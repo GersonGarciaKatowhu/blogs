3abbafc8b35899b807ce290a0737e3e0b453d867
GersonGarciaKatowhu <garcia.roman.gerson@gmail.com>
2023-04-04 12:05:16 -0600

    tercer commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,7 +10,7 @@ function About() {
         <h1 className="no-margin">Sobre mi</h1>
         <div className="padding-general">
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, animi. Id quisquam corrupti deserunt, nihil laboriosam veritatis placeat aliquid, illo adipisci vel doloribus quaerat, sequi officiis quis ut unde dolorum?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, animi. Id quisquam corrupti deserunt, nihil laboriosam veritatis placeat aliquid, illo adipisci vel doloribus quaerat, sequi officiis quis ut unde dolorum? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium optio incidunt doloremque! Rerum eum iusto, ipsa, dolorum architecto recusandae similique quos laudantium reprehenderit ea saepe magnam? Quisquam officia eum iusto.
           </p>
         </div>
       </main>
